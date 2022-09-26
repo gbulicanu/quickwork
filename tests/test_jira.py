cd1b63b0f03bc7dcb5f31fb6568920404c6df38d
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-09-27 00:05:08 +0300

    Refactor tests

--- tests/test_jira.py
+++ tests/test_jira.py
@@ -1,6 +1,7 @@
 """ JIRA tests module.
 """
 import json
+import pytest
 
 from pytest_mock import MockerFixture
 from requests_mock import Mocker
@@ -8,24 +9,20 @@ from requests_mock import Mocker
 from app.environment import JIRA_ENDPOINT
 from app.jira import execute_jql
 
-def test_execute_jql_default(mocker: MockerFixture, requests_mock: Mocker):
+@pytest.mark.parametrize(["from_days", "expected_jql"], [
+    (None, "worklogAuthor = currentUser() AND worklogDate >= startOfDay()"),
+    (0, "worklogAuthor = currentUser() AND worklogDate >= startOfDay()"),
+])
+
+def test_execute_jql(from_days, expected_jql, mocker: MockerFixture, requests_mock: Mocker):
     """ Test execute_jql default.
     """
     response_text = "{}"
-    requests_mock.request(
+    request_matcher = requests_mock.request(
         method="POST",
         url=JIRA_ENDPOINT,
         text=response_text)
-    mocker.patch("logging.info")
-    assert execute_jql() == json.loads(response_text)
-
-def test_execute_jql_zero(mocker: MockerFixture, requests_mock: Mocker):
-    """ Test execute_jql zero days.
-    """
-    response_text = "{}"
-    requests_mock.request(
-        method="POST",
-        url=JIRA_ENDPOINT,
-        text=response_text)
-    mocker.patch("logging.info")
-    assert execute_jql(from_days=0) == json.loads(response_text)
+    logging_info_patcher = mocker.patch("logging.info")
+    assert execute_jql(from_days) == json.loads(response_text)
+    assert request_matcher.last_request.json()["jql"] == expected_jql
+    logging_info_patcher.assert_called_with("%s:'%s' jql executed", "app.jira", expected_jql)
