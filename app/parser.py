54bd0e2798d5f745d31a70f7c29788a5fbfb1348
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-09-28 11:03:14 +0300

    Cover parser.py module
    Add code coverage fro parser module. Using pybars3 to simplify jira
    api results building for test scenarios.

--- app/parser.py
+++ app/parser.py
@@ -4,6 +4,8 @@ from datetime import date, datetime, timedelta
 
 from jsonpath_ng.ext import parse
 
+from app.environment import JIRA_USER
+
 
 def __filter_worklog(current, from_date):
     current_dt = datetime.strptime(current["started"], '%Y-%m-%dT%H:%M:%S.%f%z')
@@ -20,7 +22,7 @@ def process_jira(jira_result, days=-1):
         issue = [match.value for match \
             in parse(f"$.issues[?(@.key='{issue_key}')]").find(jira_result)][0]
         json_path_worklogs = parse(
-            f"$.fields.worklog.worklogs[?(@.author.emailAddress='gheorghe@gocontractor.com' \
+            f"$.fields.worklog.worklogs[?(@.author.emailAddress='{JIRA_USER}' \
               & @.started >= '{from_time}')]")
         json_path_time_spends = parse("timeSpentSeconds")
         json_path_comment = parse("comment.content[0].content[0].text")
@@ -31,15 +33,18 @@ def process_jira(jira_result, days=-1):
             comment = ([match.value for match in json_path_comment.find(worklog)] or [None])[0]
             times_spends = [match.value for match in json_path_time_spends.find(worklog)][0]
             if comment is not None and comment.strip().lower() == "pr review":
-                pr_review[issue_key]  = int(times_spends)
+                if not issue_key in pr_review:
+                    pr_review[issue_key] = int(times_spends)
+                else:
+                    pr_review[issue_key] += int(times_spends)
             else:
-                if non_pr_review.get(issue_key) is not None:
+                if issue_key in non_pr_review:
+                    comments_set, ticket_spent_time = non_pr_review[issue_key]
                     if comment is not None:
-                        comments_set, _ = non_pr_review[issue_key]
                         comments_set.add(comment.strip())
                     else:
-                        comments_set, _ = non_pr_review[issue_key]
                         comments_set.add("Missing")
+                    non_pr_review[issue_key] = (comments_set, ticket_spent_time + times_spends)
                 else:
                     if comment is not None:
                         non_pr_review[issue_key] = ({ comment.strip() }, times_spends)
