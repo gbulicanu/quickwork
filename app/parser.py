b7f080b67c412b1d775330af18620bd8f3f479da
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-09-28 11:10:41 +0300

    Refactor and fix linting

--- app/parser.py
+++ app/parser.py
@@ -14,7 +14,7 @@ def __filter_worklog(current, from_date):
 def process_jira(jira_result, days=-1):
     """ Process JIRA JSON result.
     """
-    non_pr_review = {}
+    tickets = {}
     pr_review = {}
     from_time = date.today() + timedelta(days=days)
 
@@ -24,31 +24,30 @@ def process_jira(jira_result, days=-1):
         json_path_worklogs = parse(
             f"$.fields.worklog.worklogs[?(@.author.emailAddress='{JIRA_USER}' \
               & @.started >= '{from_time}')]")
-        json_path_time_spends = parse("timeSpentSeconds")
         json_path_comment = parse("comment.content[0].content[0].text")
         worklogs = filter(
             lambda x: __filter_worklog(x, from_time),
             [match.value for match in json_path_worklogs.find(issue)])
         for worklog in worklogs:
             comment = ([match.value for match in json_path_comment.find(worklog)] or [None])[0]
-            times_spends = [match.value for match in json_path_time_spends.find(worklog)][0]
+            time_spent = [match.value for match in parse("timeSpentSeconds").find(worklog)][0]
             if comment is not None and comment.strip().lower() == "pr review":
                 if not issue_key in pr_review:
-                    pr_review[issue_key] = int(times_spends)
+                    pr_review[issue_key] = int(time_spent)
                 else:
-                    pr_review[issue_key] += int(times_spends)
+                    pr_review[issue_key] += int(time_spent)
             else:
-                if issue_key in non_pr_review:
-                    comments_set, ticket_spent_time = non_pr_review[issue_key]
+                if issue_key in tickets:
+                    comments_set, ticket_spent_time = tickets[issue_key]
                     if comment is not None:
                         comments_set.add(comment.strip())
                     else:
                         comments_set.add("Missing")
-                    non_pr_review[issue_key] = (comments_set, ticket_spent_time + times_spends)
+                    tickets[issue_key] = (comments_set, ticket_spent_time + time_spent)
                 else:
                     if comment is not None:
-                        non_pr_review[issue_key] = ({ comment.strip() }, times_spends)
+                        tickets[issue_key] = ({ comment.strip() }, time_spent)
                     else:
-                        non_pr_review[issue_key] = ({ "Missing" }, times_spends)
+                        tickets[issue_key] = ({ "Missing" }, time_spent)
 
-    return non_pr_review, pr_review
+    return tickets, pr_review
