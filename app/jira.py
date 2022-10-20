9b78684a0bc9b35ed5ab228b1886711853c21449
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-10-20 14:23:24 +0300

    Add case for default issue-key
    Relates: #21

--- app/jira.py
+++ app/jira.py
@@ -2,25 +2,27 @@
 """
 import json
 import logging
+from typing import Optional
+
 import requests
 
 from requests.auth import HTTPBasicAuth
 
 from .environment import JIRA_ENDPOINT, JIRA_TIMEOUT, JIRA_USER, JIRA_SECRET
 
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+}
 
-def execute_jql(from_days = None, max_results = 10):
+
+def execute_jql(from_days=None, max_results=10):
     """ Execute JIRA JQL against configured Atlassian jira account.
     """
     if from_days is not None and from_days >= 0:
         from_days = None
 
     auth = HTTPBasicAuth(JIRA_USER, JIRA_SECRET)
-
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
 
     from_days_or_empty = from_days or ""
     jql = f"worklogAuthor = currentUser() AND worklogDate >= startOfDay({from_days_or_empty})"
@@ -53,8 +55,45 @@ def execute_jql(from_days = None, max_results = 10):
 
     return result
 
-def add_worklog(issue_key: str, time: str, comment: str):
-    """ Add worklog to <issue_key> ticket (or to first assigned ticket if None)
+
+def add_worklog(
+    issue_key: Optional[str],
+    time: Optional[str],
+    comment: Optional[str]
+):
+    """ Add worklog to <issue_key> ticket (or to first assigned in progress ticket if None)
     """
-    logging.info("%s:add_worklog(%s, %s, %s) called",
-        __name__, issue_key, time, comment)
+
+    auth = HTTPBasicAuth(JIRA_USER, JIRA_SECRET)
+
+    if issue_key is None:
+        jql = "assignee = currentUser() AND status = 'IN PROGRESS'"
+
+        payload = json.dumps({
+            "expand": [],
+            "jql": jql,
+            "maxResults": 1,
+            "fieldsByKeys": False,
+            "fields": [
+                "summary",
+            ],
+            "startAt": 0
+        })
+
+        response = requests.request(
+            "POST",
+            JIRA_ENDPOINT,
+            data=payload,
+            headers=headers,
+            auth=auth,
+            timeout=JIRA_TIMEOUT
+        )
+
+        logging.info("%s:current_issue_key-json:%s",
+                     __name__, json.dumps(json.loads(response.text),
+                                          sort_keys=True, indent=4, separators=(",", ": ")))
+        issue_key_json = json.loads(response.text)
+        issue_key = issue_key_json["issues"][0]["key"]
+
+    logging.info("%s:add_worklog(%s, %s, %s) executing",
+                 __name__, issue_key, time, comment)
---
+++ tests/utils/__init__.py
@@ -0,0 +1,24 @@
+"""
+Unit test utilities module.
+"""
+
+
+def anything(cls):
+    """
+    Matcher whose __eq__ method always returns True.
+
+    Return
+    @param cls: The cls of the arg in assert_called_with.
+    @return: A class whose __eq__ method always returns True.
+    """
+
+    class Anything(cls):
+        """ Anything
+        """
+        def __init__(self):
+            pass
+
+        def __eq__(self, other):
+            return True
+
+    return Anything()
