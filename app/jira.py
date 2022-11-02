0a9adb7facecdaf556aaedb39169d3ad08e4c317
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-11-02 12:41:50 +0200

    fix add_worklog handler

--- app/jira.py
+++ app/jira.py
@@ -1,14 +1,16 @@
 """ JIRA module.
 """
+from datetime import datetime, timezone
 import json
 import logging
+
 from typing import Optional
 
 import requests
-
 from requests.auth import HTTPBasicAuth
 
 from .environment import JIRA_ENDPOINT, JIRA_TIMEOUT, JIRA_USER, JIRA_SECRET
+from .utils import strpdelta
 
 headers = {
     "Accept": "application/json",
@@ -42,7 +44,7 @@ def execute_jql(from_days=None, max_results=10):
 
     response = requests.request(
         "POST",
-        JIRA_ENDPOINT,
+        f"{JIRA_ENDPOINT}/search",
         data=payload,
         headers=headers,
         auth=auth,
@@ -65,6 +67,10 @@ def add_worklog(
     """
 
     auth = HTTPBasicAuth(JIRA_USER, JIRA_SECRET)
+    time_delta = strpdelta(time)
+    now = datetime.now(timezone.utc)
+
+    started = now - time_delta
 
     if issue_key is None:
         jql = "assignee = currentUser() AND status = 'IN PROGRESS'"
@@ -82,18 +88,52 @@ def add_worklog(
 
         response = requests.request(
             "POST",
-            JIRA_ENDPOINT,
+            f"{JIRA_ENDPOINT}/search",
             data=payload,
             headers=headers,
             auth=auth,
             timeout=JIRA_TIMEOUT
         )
 
-        logging.info("%s:current_issue_key-json:%s",
-                     __name__, json.dumps(json.loads(response.text),
-                                          sort_keys=True, indent=4, separators=(",", ": ")))
+        # logging.info("%s:current_issue_key-json:%s",
+        #              __name__, json.dumps(json.loads(response.text),
+        #                                   sort_keys=True, indent=4, separators=(",", ": ")))
         issue_key_json = json.loads(response.text)
         issue_key = issue_key_json["issues"][0]["key"]
+        comment = "Misc" if comment == "PR Review" else comment
 
     logging.info("%s:add_worklog(%s, %s, %s) executing",
+                 __name__, issue_key, time, comment)
+
+    started_iso = started.isoformat(timespec="milliseconds")
+    payload = json.dumps({
+        "timeSpentSeconds": time_delta.total_seconds(),
+        "comment": {
+            "type": "doc",
+            "version": 1,
+            "content": [{
+                "type": "paragraph",
+                "content": [{
+                    "text": comment,
+                    "type": "text"
+                }]
+            }]
+        },
+        "started": started_iso[:-3] + started_iso[-2:]
+    })
+
+    response = requests.request(
+        "POST",
+        f"{JIRA_ENDPOINT}/issue/{issue_key}/worklog",
+        data=payload,
+        headers=headers,
+        auth=auth,
+        timeout=JIRA_TIMEOUT,
+    )
+
+    if response.status_code > 299:
+        logging.error("%s:http error: %s reason: %s body: %s",
+                __name__, response.status_code, response.reason, response.json())
+
+    logging.info("%s:add_worklog(%s, %s, %s) succeeded",
                  __name__, issue_key, time, comment)
